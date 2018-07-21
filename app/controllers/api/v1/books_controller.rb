cc6b21b43d6dfdb78ec448a2d821bb7d416eda60
izaxveta <izavetha@gmail.com>
2018-07-20 21:13:12 -0500

    Adds api endpoint for books update.

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -11,4 +11,23 @@ class Api::V1::BooksController < ApplicationController
     book = Book.find(params[:id])
     render status: 200, json: book
   end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      render status: 202, json: {
+        message: "Successfully updated book (id:#{book.id})."
+      }
+    else
+      render status: 400, json: {
+        message: "Something happened and your request could not be fulfilled, please check yourself."
+      }
+    end
+  end
+
+  private
+
+    def book_params
+      params.require(:book).permit(:title, :author)
+    end
 end
--- spec/requests/book_api_spec.rb
+++ spec/requests/book_api_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe('Book API') do
-  it('returns all books') do
+  it 'returns all books' do
     Book.create(title: 'Dead Until Dark', author: 'Charlaine Harris')
     Book.create(title: 'Club Dead', author: 'Charlaine Harris')
     Book.create(title: 'Dead as a Doornail', author: 'Charlaine Harris')
@@ -18,7 +18,7 @@ RSpec.describe('Book API') do
     expect(content['books'].first['author']).to eq('Charlaine Harris')
   end
 
-  it('returns a single book') do
+  it 'returns a single book' do
     book_1 = Book.create(title: 'Club Dead', author: 'Charlaine Harris')
     book_2 = Book.create(title: 'Dead as a Doornail', author: 'Charlaine Harris')
 
@@ -30,5 +30,19 @@ RSpec.describe('Book API') do
     expect(content['id']).to eq(book_2.id)
     expect(content['title']).to eq('Dead as a Doornail')
     expect(content['author']).to eq('Charlaine Harris')
+  end
+
+  it 'updates a single book given provided book parameters' do
+    book_1 = Book.create(title: 'Club Dead', author: 'Charlaine Harris')
+    book_2 = Book.create(title: 'Dead as a Doornail', author: 'Charlaine Harris')
+
+    put "/api/v1/books/#{book_2.id}", params: { book: { title: 'Vampire Diaries' } }
+
+    content = JSON.parse(response.body)
+    updated_book_2 = Book.find(book_2.id)
+
+    expect(response.status).to eq(202)
+    expect(content['message']).to eq("Successfully updated book (id:#{book_2.id}).")
+    expect(updated_book_2.title).to eq("Vampire Diaries")
   end
 end
