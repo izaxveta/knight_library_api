c664aec48512490507e792595a9ce5a3bbac5e22
izaxveta <izavetha@gmail.com>
2018-07-20 22:25:26 -0500

    Adds api endpoint for books destroy.

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -18,14 +18,26 @@ class Api::V1::BooksController < ApplicationController
       render status: 202, json: {
         message: "Successfully updated book (id:#{book.id})."
       }
-    else
-      render status: 400, json: {
-        message: "Something happened and your request could not be fulfilled, please check yourself."
-      }
+    else unknown_error("Something went wrong...")
     end
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    if book.delete
+      render status:  200, json: {
+        message: "Successfully deleted book (id:#{book.id})"
+      }
+    else unknown_error("Something went wrong...")
+    end
+
+  end
+
   private
+
+    def unknown_error(message)
+      render status: 400, json: { message: message }
+    end
 
     def book_params
       params.require(:book).permit(:title, :author)
--- spec/requests/book_api_spec.rb
+++ spec/requests/book_api_spec.rb
@@ -45,4 +45,20 @@ RSpec.describe('Book API') do
     expect(content['message']).to eq("Successfully updated book (id:#{book_2.id}).")
     expect(updated_book_2.title).to eq("Vampire Diaries")
   end
+
+  it 'deletes a book' do
+    book_1 = Book.create(title: 'Dead Until Dark', author: 'Charlaine Harris')
+    book_2 = Book.create(title: 'Club Dead', author: 'Charlaine Harris')
+    book_3 = Book.create(title: 'Dead as a Doornail', author: 'Charlaine Harris')
+
+    expect(Book.count).to eq(3)
+
+    delete "/api/v1/books/#{book_2.id}"
+
+    content = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(content['message']).to eq("Successfully deleted book (id:#{book_2.id})")
+    expect(Book.count).to eq(2)
+  end
 end
